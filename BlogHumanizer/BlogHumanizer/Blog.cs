717d327ec1c6366ea552b664d51aec9df6d5f218
Ralph van der Neut <ralph.van.der.neut@zenvoices.com>
2017-08-25 20:03:22 +0200

    Added examples humanizer

---
+++ BlogHumanizer/BlogHumanizer/Blog.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BlogHumanizer
+{
+    public class Blog
+    {
+        public string Title { get; set; }
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+        public int Readers { get; set; }
+    }
+}
--- BlogHumanizer/BlogHumanizer/Controllers/ValuesController.cs
+++ BlogHumanizer/BlogHumanizer/Controllers/ValuesController.cs
@@ -3,42 +3,78 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Humanizer;
 
 namespace BlogHumanizer.Controllers
 {
     [Route("api/[controller]")]
     public class ValuesController : Controller
     {
+        public List<Blog> blogs;
+
+        public ValuesController()
+        {
+            blogs = new List<Blog>
+            {
+                new Blog()
+                {
+                    Title ="MOET IK ECHT KOMEN ?",
+                    Date = DateTime.UtcNow - 10.Hours(),
+                    Description = "In de meeste beroepen ben je dagelijks op je werk. Je komt s’ochtends aan, neemt een bak koffie en je vertrekt weer als je 8 uur hebt gewerkt ! Elke dag ben je een half uur tot een uur kwijt die tijd kan je ook aan gezin, familie of vrienden kunnen besteden.",
+                    Readers = 100
+                },
+                new Blog()
+                {
+                    Title = "GottaCleanEmAll",
+                            Date = DateTime.UtcNow - 10.Days() + 8.Hours(),
+                            Description = "Het is alweer even geleden dat ik het over machine learning heb gehad ! Het wordt tijd dat ik er mee verder ga.",
+                            Readers = 1000
+                },
+                new Blog()
+                {
+            Title = "Meneer van Dalen wacht op antwoord!",
+                            Date = DateTime.UtcNow - 30.Seconds() + 1.Weeks(),
+                            Description = "Opeens viel het kwartje vrijdag! ",
+                            Readers = 1
+                },
+                new Blog()
+                {
+                    Title = "The Expandables",
+                    Date = DateTime.UtcNow,
+                    Description = "Dit is alweer de laatste blog over de basis data structuren. We behandelen de broertjes short en long van de int en het zusje van de string char.",
+                    Readers =666
+                }
+                ,
+                new Blog()
+                {
+                Title = "nieuwe blog",
+                    Date = DateTime.UtcNow - 5.Weeks() + 36.Hours(),
+                    Description = "In de meeste beroepen ben je dagelijks op je werk. Je komt s’ochtends aan, neemt een bak koffie en je vertrekt weer als je 8 uur hebt gewerkt ! Elke dag ben je een half uur tot een uur kwijt die tijd kan je ook aan gezin, familie of vrienden kunnen besteden.",
+                    Readers = 54
+                }
+            };
+
+        }
+
         // GET api/values
         [HttpGet]
-        public IEnumerable<string> Get()
+        public IEnumerable<dynamic> Get()
         {
-            return new string[] { "value1", "value2" };
+            DateTime First = DateTime.UtcNow;
+
+            return blogs.Select(x =>
+                    new {
+                        Date = x.Date.Humanize(),
+                        Description = x.Description.Truncate(150),
+                        title = x.Title.Humanize(),
+                        TimePostedFromNow = (First - x.Date).Humanize(),
+                        readers = x.Readers.ToWords(),
+                        GreekReader = (x.Readers + 10).ToRoman(),
+                        normalGreekReaders = (x.Readers + 10).ToRoman().FromRoman().ToOrdinalWords(),
+                        RoundedReaders = (x.Readers + 1000).ToMetric()
+                    });
         }
 
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
 
-        // POST api/values
-        [HttpPost]
-        public void Post([FromBody]string value)
-        {
-        }
-
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
     }
 }
